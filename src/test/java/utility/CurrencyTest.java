5bb77a066605d3d933fc087c05f05bd81c68112b
pasakorn <pasakorn_kunchai@cmu.ac.th>
2017-03-13 17:53:03 +0700

    Add more test cases

--- src/test/java/utility/CurrencyTest.java
+++ src/test/java/utility/CurrencyTest.java
@@ -11,6 +11,12 @@ public class CurrencyTest {
     @Test
     public void testExchange(){
         Currency currency = new Currency();
-        assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1,0.001));
+        assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1.0,0.001));
+        assertThat(currency.exchange("USD",100.0,"EUR"),closeTo(101.0,0.001));
+        assertThat(currency.exchange("EUR",53.0,"USD"),closeTo(52.475,0.001));
+        assertThat(currency.exchange("THB",99.0,"EUR"),closeTo(3.03,0.001));
+        assertThat(currency.exchange("EUR",50.0,"USD"),closeTo(49.504950,0.001));
+        assertThat(currency.exchange("USD",75.0,"THB"),closeTo(2475.0,0.001));
+        assertThat(currency.exchange("USD",150.0,"EUR"),closeTo(151.5,0.001));
     }
 }
