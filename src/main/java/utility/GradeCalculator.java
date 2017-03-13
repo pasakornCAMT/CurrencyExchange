0411a0b5331cede046c0e9240c5109e3db137939
pasakorn <pasakorn_kunchai@cmu.ac.th>
2017-03-13 18:21:31 +0700

    Update calculate method step1; input summation

--- src/main/java/utility/GradeCalculator.java
+++ src/main/java/utility/GradeCalculator.java
@@ -5,6 +5,10 @@ package utility;
  */
 public class GradeCalculator {
     public char calculate(double... scores){
+        double sum = 0;
+        for (int i = 0 ; i < scores.length ; i++) {
+            sum = sum + scores[i];
+        }
         return '\0';
     }
 }
