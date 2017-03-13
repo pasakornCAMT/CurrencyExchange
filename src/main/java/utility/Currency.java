20e0eb030d9e4bd1b4d76694ac055f51719a4ef7
pasakorn <pasakorn_kunchai@cmu.ac.th>
2017-03-13 17:39:07 +0700

    Create the stub file currency

---
+++ src/main/java/utility/Currency.java
@@ -0,0 +1,10 @@
+package utility;
+
+/**
+ * Created by PASAKORN on 13/3/2560.
+ */
+public class Currency {
+    public double exchange(String sourceCurrency, double amount, String targetCurrency){
+        return 0.0;
+    }
+}
